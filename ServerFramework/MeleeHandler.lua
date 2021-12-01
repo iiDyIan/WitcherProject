cc24e31ae9c3968c02a0622869945b1985ce24a1
Dylan <94397653+iiDyIan@users.noreply.github.com>
2021-11-30 22:30:05 -0500

    Update MeleeHandler.lua

--- ServerFramework/MeleeHandler.lua
+++ ServerFramework/MeleeHandler.lua
@@ -1,65 +1,132 @@
 local module = {}
 
-local statusDict= {}
+local statusDict = {}
 
-function module.init(User)
+local animationModule = require(script.Parent.Parent:WaitForChild('AnimationHandler'))
 
-    -- m6D where intended
+local remoteStorage = game:GetService("ReplicatedStorage"):WaitForChild("EncryptedFunctions")
 
-    statusDict[User] = 1
+local remote1 = remoteStorage:WaitForChild("LightAttack")
+local remote2 = remoteStorage:WaitForChild("HeavyAttack")
 
-end
+local remote3 = remoteStorage:WaitForChild("LightAttack")
+local remote4 = remoteStorage:WaitForChild("HeavyAttack")
 
-function module.StartEquip(User)
+local remote5 = remoteStorage:WaitForChild("Parry")
 
-    if statusDict[User] ~= 1 then return end
+function module.init(sendModule, character, weapon)
+	
+	local torso = character:WaitForChild("UpperTorso")
 
-    statusDict[User] = 2
+	local primary = weapon:Clone()
 
-    -- call animation module for equip
+	primary.Parent = character
+	primary.Name = "Primary"
 
-    -- update m6D weld
+	local m6d = script.Primary:Clone()
 
-    statusDict[User] = 3
+	m6d.Part0 = character.UpperTorso
+	m6d.Part1 = primary.Primary
 
-    -- call animation module for loop
+	m6d.Parent = character.UpperTorso
+	m6d.Name = "Primary"
 
-end
-
-function module.StartUnequip(User)
-
-    if statusDict[User] ~= 3 then return end
-
-    statusDict[User] = 4
-
-    -- stop running animations
-
-    -- call animation module for unequip
-
-    statusDict[User] = 1
+	statusDict[character.Name] = 1
 
 end
 
-function module.EngageHeavy(User)
+function module.StartEquip(character)
 
-    if not statusDict[User] == 3 then return end
+	if statusDict[character.Name] ~= 1 then return end
 
+	statusDict[character.Name] = 2
 
+	if not character:FindFirstChild("Primary") then return "Primary weapon not identified." end
+
+	local weapon = character:FindFirstChild("Primary")
+
+	animationModule:PlayAnimation(character.Humanoid, "SwordClass", "Equip")
+
+	wait(18/60)
+
+	if character:FindFirstChild("UpperTorso") then
+		if character:FindFirstChild("UpperTorso"):FindFirstChild("Primary") then
+			character:FindFirstChild("UpperTorso"):FindFirstChild("Primary"):Destroy()
+		end
+	end
+
+	local equipM6D = script.EquipPrimary:Clone()
+
+	equipM6D.Part0 = character:FindFirstChild("RightHand")
+	equipM6D.Part1 = weapon.Primary	
+
+	equipM6D.Parent = character:FindFirstChild("RightHand")
+
+	wait(18/60)
+
+	animationModule:PlayAnimation(character.Humanoid, "SwordClass", "Idle")
+	
+	statusDict[character.Name] = 3
+
+	return "Weapon equipped."
 
 end
 
-function module.EngageLight(User)
+function module.StartUnequip(character)
 
-    if not statusDict[User] == 3 then return end
+	if statusDict[character.Name] ~= 3 then return end
 
+	statusDict[character.Name] = 4
+	
+	if not character:FindFirstChild("Primary") then return "Primary weapon not identified." end
+
+	local weapon = character:FindFirstChild("Primary")
+
+	animationModule:PlayAnimation(character.Humanoid, "SwordClass", "Unequip")
+
+	wait(18/60)
+
+	if character:FindFirstChild("RightHand") then
+		if character:FindFirstChild("RightHand"):FindFirstChild("EquipPrimary") then
+			character:FindFirstChild("RightHand"):FindFirstChild("EquipPrimary"):Destroy()
+		end
+	end
+
+	local primaryM6D = script.Primary:Clone()
+
+	primaryM6D.Part0 = character:FindFirstChild("UpperTorso")
+	primaryM6D.Part1 = weapon.Primary	
+
+	primaryM6D.Parent = character:FindFirstChild("UpperTorso")
+
+	wait(19/60)
+
+	animationModule:StopAnimation(character.Humanoid, "SwordClass", "Idle")
+	
+	statusDict[character.Name] = 1
+	
+	return "Weapon unequipped."
+	
+end
+
+function module.EngageHeavy(character)
+
+	if not statusDict[character.Name] == 3 then return end
+
+	
 
 end
 
-function module.EngageParry(User)
+function module.EngageLight(character)
 
-    if not statusDict[User] == 3 then return end
+	if not statusDict[character.Name] == 3 then return end
 
-    
+end
+
+function module.EngageParry(character)
+
+	if not statusDict[character.Name] == 3 then return end
+
 
 end
 
